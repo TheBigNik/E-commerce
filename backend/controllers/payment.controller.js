c7de35f594a3b5c1512bb1c84e15d29e294743d2
TheBigNik <hosseinniknejad81@gmail.com>
2026-02-06 05:45:52 +0330

    optimizing the backend

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -1,4 +1,4 @@
-import { stripe } from "../lib/stripe";
+// import { stripe } from "../lib/stripe.js";
 import Coupon from "../models/coupon.model.js";
 import Order from "../models/order.model.js";
 import router from "../routes/cart.route.js";
@@ -43,90 +43,88 @@ export const createCheckoutSession = async (req, res) => {
       }
     }
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      line_items: lineItems,
-      mode: "payment",
-      success_url: `${process.env.CLIENT_URL}/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${process.env.CLIENT_URL}/purchase-cancel`,
-      discounts: coupon
-        ? [
-            {
-              coupon: await createStipeCoupon(coupon.discountPercentage),
-            },
-          ]
-        : [],
-      metadata: {
-        userId: req.user._id.toString(),
-        couponCode: couponCode || "",
-        products: JSON.stringify(
-            products.map((p) => ({
-                id: p.id,
-                quantity: p.quantity,
-                price: p.price
-            }))
-        )
-      },
-    });
+    // const session = await stripe.checkout.sessions.create({
+    //   payment_method_types: ["card"],
+    //   line_items: lineItems,
+    //   mode: "payment",
+    //   success_url: `${process.env.CLIENT_URL}/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
+    //   cancel_url: `${process.env.CLIENT_URL}/purchase-cancel`,
+    //   discounts: coupon
+    //     ? [
+    //         {
+    //           coupon: await createStipeCoupon(coupon.discountPercentage),
+    //         },
+    //       ]
+    //     : [],
+    //   metadata: {
+    //     userId: req.user._id.toString(),
+    //     couponCode: couponCode || "",
+    //     products: JSON.stringify(
+    //         products.map((p) => ({
+    //             id: p.id,
+    //             quantity: p.quantity,
+    //             price: p.price
+    //         }))
+    //     )
+    //   },
+    // });
 
     if (totalAmount >= 20000) {
       await createNewCoupon(req.user._id);
     }
     res.status(200).json({ id: session.id, totalAmount: totalAmount / 100 });
   } catch (error) {}
-
-  async function createStipeCoupon(discountPercentage) {
-    const coupon = await stripe.coupons.create({
-      percent_off: discountPercentage,
-      duration: "once",
-    });
-
-    return coupon.id;
-  }
 };
 
-export const checkoutSuccess = async (req, res) => {
-  try {
-    const { sessionId } = req.body;
-    const session = await stripe.checkout.sessions.retrieve(sessionId);
+// export const checkoutSuccess = async (req, res) => {
+//   try {
+//     const { sessionId } = req.body;
+//     const session = await stripe.checkout.sessions.retrieve(sessionId);
 
-    if (session.payment_status === "paid") {
-      if (session.metadata.couponCode) {
-        await Coupon.findOneAndUpdate(
-          {
-            code: session.metadata.couponCode,
-            userId: session.metadata.userId,
-          },
-          {
-            isActive: false,
-          });
-      }
+//     if (session.payment_status === "paid") {
+//       if (session.metadata.couponCode) {
+//         await Coupon.findOneAndUpdate(
+//           {
+//             code: session.metadata.couponCode,
+//             userId: session.metadata.userId,
+//           },
+//           {
+//             isActive: false,
+//           },
+//         );
+//       }
 
-      const products = JSON.parse(session.metadata.products)
-      const newOrder = new Order({
-        user:session.metadata.userId,
-        products: products.map(product => ({
-            product: product.id,
-            quantity: product.quantity,
-            price: product.price
-        })),
-        totalAmount: session.amount_total / 100,
-        stripeSessionId: sessionId
-      })
+//       const products = JSON.parse(session.metadata.products);
+//       const newOrder = new Order({
+//         user: session.metadata.userId,
+//         products: products.map((product) => ({
+//           product: product.id,
+//           quantity: product.quantity,
+//           price: product.price,
+//         })),
+//         totalAmount: session.amount_total / 100,
+//         stripeSessionId: sessionId,
+//       });
 
-      await newOrder.save()
+//       await newOrder.save();
 
-      res.status(200).json({
-        success: true,
-        message: "Payment successful, order created, and coupon deactivated if used.",
-        orderId: newOrder._id
-      })
-    }
-  } catch (error) {
-    console.log("Error processing successful checkout:", error)
-    res.status(500).json({ message: "Error processing successful checkout", error: error.message})
-  }
-};
+//       res.status(200).json({
+//         success: true,
+//         message:
+//           "Payment successful, order created, and coupon deactivated if used.",
+//         orderId: newOrder._id,
+//       });
+//     }
+//   } catch (error) {
+//     console.log("Error processing successful checkout:", error);
+//     res
+//       .status(500)
+//       .json({
+//         message: "Error processing successful checkout",
+//         error: error.message,
+//       });
+//   }
+// };
 
 async function createNewCoupon(userId) {
   const newCoupon = new Coupon({
@@ -140,3 +138,13 @@ async function createNewCoupon(userId) {
 
   return newCoupon;
 }
+
+//   async function createStipeCoupon(discountPercentage) {
+//     const coupon = await stripe.coupons.create({
+//       percent_off: discountPercentage,
+//       duration: "once",
+//     });
+
+//     return coupon.id;
+//   }
+// }
--- backend/lib/stripe.js
+++ backend/lib/stripe.js
@@ -1,7 +1,7 @@
-import Stripe from "stripe";
-import dotenv from "dotenv"
+// import Stripe from "stripe";
+// import dotenv from "dotenv"
 
-dotenv.config()
+// dotenv.config()
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+// export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
